92a818e02b10f41591fd37389b6e4a8f801e9291
Micah Hahn <micah@noredink.com>
2022-06-01 20:22:33 -0400

    Let fold use the TCO

--- src/Elm/Syntax/Expression/Extra.elm
+++ src/Elm/Syntax/Expression/Extra.elm
@@ -4,67 +4,70 @@ import Elm.Syntax.Expression exposing (Expression(..), LetDeclaration(..))
 import Elm.Syntax.Node as Node exposing (Node(..))
 
 
+foldHelper : (Node Expression -> a -> a) -> a -> List (Node Expression) -> a
+foldHelper function accum stack =
+    case stack of
+        [] ->
+            accum
+
+        expr :: stackTail ->
+            let
+                newStack =
+                    case Node.value expr of
+                        Application exprs ->
+                            exprs
+
+                        OperatorApplication _ _ leftExp rightExp ->
+                            [ leftExp, rightExp ]
+
+                        IfBlock condExp trueExp falseExp ->
+                            [ condExp, trueExp, falseExp ]
+
+                        Negation exp ->
+                            [ exp ]
+
+                        TupledExpression exps ->
+                            exps
+
+                        ParenthesizedExpression exp ->
+                            [ exp ]
+
+                        LetExpression { declarations, expression } ->
+                            let
+                                mapLetDeclarations (Node _ letDeclaration) =
+                                    case letDeclaration of
+                                        LetFunction { declaration } ->
+                                            (Node.value declaration).expression
+
+                                        LetDestructuring _ exp ->
+                                            exp
+                            in
+                            List.map mapLetDeclarations declarations ++ [ expression ]
+
+                        CaseExpression { expression, cases } ->
+                            expression :: List.map Tuple.second cases
+
+                        LambdaExpression { expression } ->
+                            [ expression ]
+
+                        RecordExpr recordSetters ->
+                            List.map (Node.value >> Tuple.second) recordSetters
+
+                        ListExpr exps ->
+                            exps
+
+                        RecordAccess exp _ ->
+                            [ exp ]
+
+                        RecordUpdateExpression _ recordSetters ->
+                            List.map (Node.value >> Tuple.second) recordSetters
+
+                        _ ->
+                            []
+            in
+            foldHelper function (function expr accum) (newStack ++ stackTail)
+
+
 fold : (Node Expression -> a -> a) -> a -> Node Expression -> a
 fold function accum expr =
-    let
-        newAccum =
-            function expr accum
-
-        -- Reorder arguments for easy passing into List.foldl
-        folder elem a =
-            fold function a elem
-    in
-    case Node.value expr of
-        Application exprs ->
-            List.foldl folder newAccum exprs
-
-        OperatorApplication _ _ leftExp rightExp ->
-            List.foldl folder newAccum [ leftExp, rightExp ]
-
-        IfBlock condExp trueExp falseExp ->
-            List.foldl folder newAccum [ condExp, trueExp, falseExp ]
-
-        Negation exp ->
-            folder exp newAccum
-
-        TupledExpression exps ->
-            List.foldl folder newAccum exps
-
-        ParenthesizedExpression exp ->
-            folder exp newAccum
-
-        LetExpression { declarations, expression } ->
-            let
-                mapLetDeclarations (Node _ letDeclaration) =
-                    case letDeclaration of
-                        LetFunction { declaration } ->
-                            (Node.value declaration).expression
-
-                        LetDestructuring _ exp ->
-                            exp
-
-                declAccum =
-                    List.foldl (mapLetDeclarations >> folder) newAccum declarations
-            in
-            folder expression declAccum
-
-        CaseExpression { expression, cases } ->
-            List.foldl (Tuple.second >> folder) (folder expression newAccum) cases
-
-        LambdaExpression { expression } ->
-            folder expression newAccum
-
-        RecordExpr recordSetters ->
-            List.foldl (Node.value >> Tuple.second >> folder) newAccum recordSetters
-
-        ListExpr exps ->
-            List.foldl folder newAccum exps
-
-        RecordAccess exp _ ->
-            folder exp newAccum
-
-        RecordUpdateExpression _ recordSetters ->
-            List.foldl (Node.value >> Tuple.second >> folder) newAccum recordSetters
-
-        _ ->
-            newAccum
+    foldHelper function accum [ expr ]
