a2cd92a79d1936d843c1945c70eb4a3e553bbfcf
Micah Hahn <micah@noredink.com>
2022-06-13 17:04:34 -0400

    Provide docs for Elm.Syntax.Range.Extra

--- src/Elm/Syntax/Range/Extra.elm
+++ src/Elm/Syntax/Range/Extra.elm
@@ -1,4 +1,10 @@
-module Elm.Syntax.Range.Extra exposing (recoverRange, recoverRanges)
+module Elm.Syntax.Range.Extra exposing (lookupRange, lookupRanges)
+
+{-| Provides helpers for working with Ranges produced by `elm-syntax`
+
+@docs lookupRange, lookupRanges
+
+-}
 
 import Array exposing (Array)
 import Elm.Syntax.Range exposing (Range)
@@ -6,46 +12,29 @@ import Elm.Syntax.Range exposing (Range)
 
 indexLineStarts : String -> Array Int
 indexLineStarts string =
-    indexLineStartsHelper string 0 []
+    indexLineStartsHelper (String.toList string) 0 []
         |> List.reverse
         |> (::) 0
         |> Array.fromList
 
 
-
--- Will not be the fastest approach, but the String module doesn't
--- really give me the tools I need to do this properly
-
-
-indexLineStartsHelper : String -> Int -> List Int -> List Int
+indexLineStartsHelper : List Char -> Int -> List Int -> List Int
 indexLineStartsHelper string index accum =
-    if String.isEmpty string then
-        accum
+    case string of
+        '\u{000D}' :: '\n' :: rest ->
+            indexLineStartsHelper rest (index + 2) (index + 2 :: accum)
 
-    else if String.startsWith "\u{000D}\n" string then
-        indexLineStartsHelper (String.dropLeft 2 string) (index + 2) (index + 2 :: accum)
+        '\n' :: rest ->
+            indexLineStartsHelper rest (index + 1) (index + 1 :: accum)
 
-    else if String.startsWith "\n" string || String.startsWith "\u{000D}" string then
-        indexLineStartsHelper (String.dropLeft 1 string) (index + 1) (index + 1 :: accum)
+        '\u{000D}' :: rest ->
+            indexLineStartsHelper rest (index + 1) (index + 1 :: accum)
 
-    else
-        indexLineStartsHelper (String.dropLeft 1 string) (index + 1) accum
-
-
-recoverHelper : String -> Array Int -> Range -> Maybe String
-recoverHelper source rowStarts { start, end } =
-    case ( Array.get (start.row - 1) rowStarts, Array.get (end.row - 1) rowStarts ) of
-        ( Just startIndex, Just endIndex ) ->
-            String.slice (startIndex + start.column - 1) (endIndex + end.column - 1) source
-                |> Just
+        _ :: rest ->
+            indexLineStartsHelper rest (index + 1) accum
 
         _ ->
-            Nothing
-
-
-recoverRange : String -> Range -> Maybe String
-recoverRange source range =
-    recoverHelper source (indexLineStarts source) range
+            accum
 
 
 {-| Borrowed from elm-community/maybe-extra
@@ -70,10 +59,41 @@ combineHelp list acc =
             Just (List.reverse acc)
 
 
-{-| Will be faster than recoverRange when recovering multiple ranges from the same text
+recoverHelper : String -> Array Int -> Range -> Maybe String
+recoverHelper source rowStarts { start, end } =
+    case ( Array.get (start.row - 1) rowStarts, Array.get (end.row - 1) rowStarts ) of
+        ( Just startIndex, Just endIndex ) ->
+            String.slice (startIndex + start.column - 1) (endIndex + end.column - 1) source
+                |> Just
+
+        _ ->
+            Nothing
+
+
+{-| Lookup the string pointed to by a range in the source it was originally generated from.
+
+    lookupRange "1 + 2"
+        { start =
+            { row = 1, column = 1 }
+        , end =
+            { row = 1, column = 2 }
+        }
+        == "1"
+
+Note that if the range is out of bounds of the source text `lookupRange` will return nothing.
+This can only happen if the range was not produced from the provided source text.
+
 -}
-recoverRanges : String -> List Range -> Maybe (List String)
-recoverRanges source ranges =
+lookupRange : String -> Range -> Maybe String
+lookupRange source range =
+    recoverHelper source (indexLineStarts source) range
+
+
+{-| Use this instead of `recoverRange` when trying to recover multiple ranges from the same source text.
+It only indexes the source once and thus will be much more efficient.
+-}
+lookupRanges : String -> List Range -> Maybe (List String)
+lookupRanges source ranges =
     let
         rowStarts =
             indexLineStarts source
